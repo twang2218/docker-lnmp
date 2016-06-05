f941d867c75176cf1aba1617849c4665ec95fabd
Tao Wang <twang2218@gmail.com>
2016-06-06 02:12:00 +1000

    给PHP引擎添加 mysql 支持

--- site/index.php
+++ site/index.php
@@ -1,1 +1,20 @@
-<?php phpinfo();?>
+<?php
+
+
+
+$servername = "mysql";
+$username = "root";
+$password = "Passw0rd";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+echo "Connected MySQL successfully";
+
+phpinfo();
+
+?>
